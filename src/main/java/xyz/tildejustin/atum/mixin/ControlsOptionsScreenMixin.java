40742905c7dfb3a9863c2c451c3740404db9e871
tildejustin <spacepepper3.14@gmail.com>
2023-03-16 19:04:16 -0400

    finished :pray:

--- src/main/java/xyz/tildejustin/atum/mixin/ControlsOptionsScreenMixin.java
+++ src/main/java/xyz/tildejustin/atum/mixin/ControlsOptionsScreenMixin.java
@@ -1,14 +1,16 @@
 package xyz.tildejustin.atum.mixin;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.options.ControlsOptionsScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(ControlsOptionsScreen.class)
-public abstract class ControlsOptionsScreenMixin {
-    @ModifyArg(method = "init", at = @At(value = "", target = ""), index = 1)
-    lowerOptionsDoneButton() {
-
+public abstract class ControlsOptionsScreenMixin extends Screen {
+    @ModifyArg(method = "init", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/ButtonWidget;<init>(IIILjava/lang/String;)V"), index = 2)
+    private int lowerOptionsDoneButton(int num) {
+        return this.height / 6 + 24 * (((Minecraft.getMinecraft().options.allKeys.length + 2 - 1) / 2));
     }
 }
